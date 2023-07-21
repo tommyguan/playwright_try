f36e4f2390ecee74d1aeccd5994c4a3ac34cac3d
Tommy Guan <tommy.guan@oracle.com>
2023-07-21 12:35:47 -0700

    test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,12 +7,3 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
--- tests/test-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-  await page.getByRole('combobox', { name: 'Search' }).fill('test');
-  await page.getByRole('combobox', { name: 'Search' }).press('Enter');
-  await expect(page.getByRole('link', { name: 'Images' })).toHaveCount(2);
-
-});
