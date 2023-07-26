4406d211b20fdc5e06a521446ec76315e56947e9
Tommy Guan <tommy.guan@oracle.com>
2023-07-26 11:56:40 -0700

    test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { chromium, test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("Logger", async ({page}) => {
+    // const browser = await chromium.launch({
+    //     logger: {
+    //         isEnabled: (name, severity) => true,
+    //         log: (name, severity, message, args) => console.log(`name = ${name} \n msg = ${message} \n severity = ${severity} \n args = ${args}`)
+    //     }
+    // });
+    // const context = await browser.newContext();
+    // const page = await context.newPage();
+    //const consoleLogs = [];
+    page.on("console", msg => {
+        console.log(msg.text());
+  
+        
+    })
+    await page.goto("https://letcode.in/elements");
+    const btn = page.locator("#search");
+    await btn.click();
+    
+    await page.goto("https://www.amazon.in/asas")
+    await page.waitForTimeout(3000);
+    // write the logs to the winston logger
 });
 
